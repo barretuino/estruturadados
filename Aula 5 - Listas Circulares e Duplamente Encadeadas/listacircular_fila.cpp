c3787917fb0721571fb5b4ad9ceef1b91e97d2bb
Barretuino Tecnologia <41486280+barretuino@users.noreply.github.com>
2023-08-31 10:07:21 -0300

    Add files via upload

---
+++ Aula 5 - Listas Circulares e Duplamente Encadeadas/listacircular_fila.cpp
@@ -0,0 +1,84 @@
+/* Lista Circular utilizando Fila*/
+
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+struct no
+{
+  int info;
+  struct no *prox;
+};
+
+struct no* insere(struct no **fila, int x)
+{
+  struct no *novo;
+  novo = (struct no*)malloc(sizeof(struct no));
+  novo->info = x;
+  if(*fila == NULL)
+    *fila = novo;
+  else
+    novo->prox = (*fila)->prox;
+  (*fila)->prox = novo;
+  *fila = novo;
+}
+
+int remove(struct no **fila)
+{
+  int x;
+  struct no *aux;
+  if(*fila == NULL)
+  {
+    printf("Fila Vazia\n");
+    return (-1);
+  } 
+  else
+  {
+    aux = (*fila)->prox;
+    x = aux->info;
+    if(aux == *fila)
+      //somente um nó da fila
+      *fila = NULL;
+    else
+      (*fila)->prox = aux->prox;
+    free(aux);
+    return(x);
+  }
+}
+
+main()
+{
+  int x, op, retorno;
+  struct no *fila;
+  fila = NULL;
+  
+  while(op!=3)
+  {
+    system("cls");
+    printf("\nManipulação de Lista Circular usando Fila");
+    printf("\nDigite 1 para inserir");
+    printf("\nDigite 2 para excluir");
+    printf("\nDigite 3 para sair");
+    printf("\nEntre com uma opcao: ");
+    scanf("%i",&op);
+    
+    switch(op)
+    {
+      case 1:
+           printf("Entre com um valor inteiro: ");
+           scanf("%i",&x);
+           insere(&fila, x);
+           break;
+      case 2:
+           retorno = remove(&fila);
+           if(retorno != -1)
+             printf("\nO valor de retorno foi: %d",retorno);
+           else
+             printf("\nTentou remover de uma Fila vazia");
+           getche();
+           break;
+      case 3:
+           exit(1); 
+     }
+   }
+}
